003144552826b144c1bbcad300e3abf3cbafa098
Joao vitor <joaosoares07122004@gmail.com>
2024-09-29 16:31:14 -0300

    :feat: Adicionando funcionalidade ao botão de reiniciar

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-
-
-
-
 function sortear(){
     let quantidadeDeNumeros = parseInt(document.getElementById('quantidade').value);
     let minimo = parseInt(document.getElementById('de').value);
@@ -17,6 +13,7 @@ function sortear(){
 
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${listaDeNumeros}</label>`;
+    alterarStatusDoBotao();
 }
 
 function gerarNumerosAleatorios(min , max) {
@@ -24,4 +21,30 @@ function gerarNumerosAleatorios(min , max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 
             }
+
+
+function alterarStatusDoBotao() {
+
+        let botao = document.getElementById('btn-reiniciar');
+
+        if (botao.classList.contains('container__botao-desabilitado')) {
+
+            botao.classList.remove('container__botao-desabilitado');
+            botao.classList.add('container__botao');
+
+        } else {
+
+            botao.classList.remove('container__botao');
+            botao.classList.add('container__botao-desabilitado');
+
+        }
+            }
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '';
+    alterarStatusDoBotao();
+}
 
